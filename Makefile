5907b470c10e15a30ba20930270e8f12b57f0e77
polina <polovnikovapo@mail.ru>
2023-05-06 23:34:22 +0700

    Запуск через docker compose (#14)

---
+++ Makefile
@@ -0,0 +1,15 @@
+default: pull start
+
+pull:
+    git pull --rebase
+
+start:
+    docker compose build
+    docker compose down
+    docker compose up -d
+
+down:
+    docker compose down
+
+logs:
+    docker compose logs -f
--- back/slides/settings.py
+++ back/slides/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'slides.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DATABASE"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DATABASE", "slides"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "252426"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     },
 }
 
